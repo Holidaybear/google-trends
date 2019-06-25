62a0df4573268a85669171fea78f8f08627ec102
Kami <yichen@mozilla.com>
2019-06-25 14:05:13 +0800

    Use local data source

--- app/src/main/java/tw/holidaybear/trends/MainActivity.kt
+++ app/src/main/java/tw/holidaybear/trends/MainActivity.kt
@@ -3,8 +3,9 @@ package tw.holidaybear.trends
 import androidx.databinding.DataBindingUtil
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
 import tw.holidaybear.trends.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +19,70 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun load() {
-        val api = TrendsAPI.create()
-        api.getTrends()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe ({ binding.trendView.playTrendList(it[40].keywords) }, {})
+        applicationContext.assets.open("trends.json").use { inputStream ->
+            JsonReader(inputStream.reader()).use { jsonReader ->
+                val trendType = object : TypeToken<Map<String, List<String>>>() {}.type
+                val trendMap: Map<String, List<String>> = Gson().fromJson(jsonReader, trendType)
+                val regionList = mutableListOf<Region>()
+                trendMap.forEach { (key, value) ->
+                    regionList.add(Region(getRegionName(key), value))
+                }
+                regionList.sortWith(compareBy { it.name })
+                binding.trendView.playTrendList(regionList[40].keywords)
+            }
+        }
+    }
+
+    private fun getRegionName(key: String): String {
+        return when(key) {
+            "30" -> "Argentina"
+            "8" -> "Australia"
+            "44" -> "Austria"
+            "41" -> "Belgium"
+            "18" -> "Brazil"
+            "13" -> "Canada"
+            "38" -> "Chile"
+            "32" -> "Colombia"
+            "43" -> "Czech Republic"
+            "49" -> "Denmark"
+            "29" -> "Egypt"
+            "50" -> "Finland"
+            "16" -> "France"
+            "15" -> "Germany"
+            "48" -> "Greece"
+            "10" -> "Hong Kong"
+            "45" -> "Hungary"
+            "3" -> "India"
+            "19" -> "Indonesia"
+            "6" -> "Israel"
+            "27" -> "Italy"
+            "4" -> "Japan"
+            "37" -> "Kenya"
+            "34" -> "Malaysia"
+            "21" -> "Mexico"
+            "17" -> "Netherlands"
+            "52" -> "Nigeria"
+            "51" -> "Norway"
+            "25" -> "Philippines"
+            "31" -> "Poland"
+            "47" -> "Portugal"
+            "39" -> "Romania"
+            "14" -> "Russia"
+            "36" -> "Saudi Arabia"
+            "5" -> "Singapore"
+            "40" -> "South Africa"
+            "23" -> "South Korea"
+            "26" -> "Spain"
+            "42" -> "Sweden"
+            "46" -> "Switzerland"
+            "12" -> "Taiwan"
+            "33" -> "Thailand"
+            "24" -> "Turkey"
+            "35" -> "Ukraine"
+            "9" -> "United Kingdom"
+            "1" -> "United States"
+            "28" -> "Vietnam"
+            else -> throw Exception("Unknown region code: $key")
+        }
     }
 }
